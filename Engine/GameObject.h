72901301f9ba624d58c81c8dbd8381c2c76c85e4
Hyup98 <40859624+Hyup98@users.noreply.github.com>
2023-07-31 15:57:27 +0900

    Terrain

--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -5,9 +5,10 @@
 class Transform;
 class MeshRenderer;
 class Camera;
-class MonoBehaviour;
 class Light;
+class MonoBehaviour;
 class ParticleSystem;
+class Terrain;
 
 class GameObject : public Object, public enable_shared_from_this<GameObject>
 {
@@ -28,6 +29,7 @@ public:
 	shared_ptr<Camera> GetCamera();
 	shared_ptr<Light> GetLight();
 	shared_ptr<ParticleSystem> GetParticleSystem();
+	shared_ptr<Terrain> GetTerrain();
 
 	void AddComponent(shared_ptr<Component> component);
 
